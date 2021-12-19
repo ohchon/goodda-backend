859dd2bc8203c0099ec4069688870277649afd1a
Sunghyun Oh <dtg8@naver.com>
2021-12-19 14:41:46 +0900

    스케쥴러 추출

--- src/main/java/com/finalproject/gooddabackend/GooddaBackendApplication.java
+++ src/main/java/com/finalproject/gooddabackend/GooddaBackendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+//@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 @PropertySource("file:/home/ubuntu/application.properties")//ec2용
